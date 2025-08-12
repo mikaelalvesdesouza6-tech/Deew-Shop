ba833f28e20181daa031862d28fb9a2f7fbe07c7
mikaelalvesdesouza6-tech <mikaelalvesdesouza6@gmail.com>
2025-08-12 02:49:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,9 @@
 
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(caches.open('deew-shop-v3').then(cache => cache.addAll([
-    './',
-    './index.html',
-    './manifest.json',
-    './icon-192.png',
-    './icon-512.png'
+  e.waitUntil(caches.open('deew-shop-v4').then(cache => cache.addAll([
+    './','./index.html','./manifest.json','./icon-192.png','./icon-512.png',
+    './dr_planta_prensado.png','./dr_planta_flor.png','./homem_gelo_ice.png','./homem_barro_dry.png'
   ])));
 });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
